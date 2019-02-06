9058302120c22878ce540f6c39b4023d09a61ae3
RUDR108 <34435910+akshit777@users.noreply.github.com>
2019-02-07 01:03:26 +0530

    something checking

--- src/components/shared/informationHeader.js
+++ src/components/shared/informationHeader.js
@@ -8,20 +8,18 @@ render(){
     <div className="container textContainer" style={{marginBottom:'20px'}}>
             Everyone wants to live somewhere.You are here for it.
     </div>
-        <div className="container-fluid ">
-        <div className="row">
-    <div className="profilePic col-md-3" style={{paddingBottom:'10px',marginBottom:'10px',float:'right',marginRight:'10px',marginLeft:'10px'}}>
+        <div className="container container__row">
+    <div className="profilePic col-md-3 container__col-3" style={{paddingBottom:'10px',marginBottom:'10px',float:'right',marginRight:'10px',marginLeft:'10px'}}>
     <img style={{height:'40px',width:'40px',borderRadius:'20px'}} src='https://api.adorable.io/avatars/285/abott@adorable.png' alt='owner'/>
     </div>
-    <div className="homeButton  headText col-md-3" onClick={this.goToHome}>
-    <p>Home </p>
+    <div className="homeButton  headText col-md-3 container__col-3" onClick={this.goToHome}>
+    <center><p>Home </p></center>
     </div>
-    <div className="searchButton headText col-md-3">
-    <p>Search</p>
+    <div className="searchButton headText col-md-3 container__col-3">
+    <center><p>Search</p></center>
     </div>
-    <div className="ChatButton headText col-md-3">
-    <p>Chat</p>
-    </div>
+    <div className="ChatButton headText col-md-3 container__col-3" style={{border:'none'}}>
+    <center><p style={{paddingLeft:'10px'}}>Chat</p></center>
     </div>
     </div>
    </div>
